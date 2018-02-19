5471f759bdec7d261af39e30fd94ceaa97a0c474
Chase <chaseit12@gmail.com>
2018-02-19 22:46:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,5 +5,10 @@ def line(arr)
       return 'The line is currently empty.'  
    end
    
+   str = 'The line is currently: '
    
+   arr.each_with_index{
+     |name, idx|
+     str += "#{(idx+1).to_s} "
+   }
 end
